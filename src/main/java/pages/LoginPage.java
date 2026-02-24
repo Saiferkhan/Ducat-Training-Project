65a2ef7e9ea278f2c855dfe4ca3c3a0de1fb1d92
Saifer Khan <saiferkhan99@gmail.com>
2026-02-24 14:02:44 +0530

    added Pagefactory class in testng

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,34 +1,43 @@
 package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 public class LoginPage {
     private final WebDriver driver;
 
     // ---------- Locators ----------
-    private final By usernameInput = By.name("username");
-    private final By passwordInput = By.name("password");
-    private final By loginBtn      = By.tagName("button");
-   // private By errorMsg      = By.cssSelector("div.error-msg");
-
+      
+    @FindBy(name="username")
+    WebElement userInput;
+    
+    @FindBy(name="password")
+    WebElement passwordInput;
+    
+    @FindBy(tagName="button")
+    WebElement loginbtn;
+    
     // ---------- Constructor ----------
     public LoginPage(WebDriver driver) {
 
         this.driver = driver;
+        PageFactory.initElements(driver,this);
     }
 
     // ---------- Page Actions ----------
     public void enterUsername(String username) {
-        driver.findElement(usernameInput).clear();
-        driver.findElement(usernameInput).sendKeys(username);
+        userInput.clear();
+        userInput.sendKeys(username);
     }
 
     public void enterPassword(String password) {
-        driver.findElement(passwordInput).clear();
-        driver.findElement(passwordInput).sendKeys(password);
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
     }
 
     public void clickLogin() {
-        driver.findElement(loginBtn).click();
+        loginbtn.click();
     }
 
     //public String getErrorMessage() {
